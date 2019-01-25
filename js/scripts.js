ae57990c127e051fa981c0dbf18eb5ebf324b797
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-01-25 09:20:20 -0800

    remove commas in list and add spaces

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
+var string
 $(function(){
 
   $("form#counter").submit(function(event){
     event.preventDefault();
     var highNumberInput = parseInt($("input#highNumber").val());
     var numberList = []
-      var listing = function(number){
+    
+    var listing = function(number){
       for (var i = 0; i <= number; i++){
         numberList.push(i);
       }
     }
     listing(highNumberInput);
 
+    $(".numberList").text(numberList.join(" "));
   });
 
 
