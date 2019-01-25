306251648d3bc34037f46a27b59234fa2088dac1
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-01-25 11:51:10 -0800

    add form styling and error messages

--- js/scripts.js
+++ js/scripts.js
@@ -15,39 +15,45 @@ $(function(){
     event.preventDefault();
 
     if ($('input#name').val() === ""){
+      $("#name").removeClass("form-control")
+      $("#name").addClass("form-control is-invalid")
+      $("#nameError").text("Please enter your name.")
+    } else if ($('input#highNumber').val() === "") {
+      $("#highNumber").removeClass("form-control")
+      $("#highNumber").addClass("form-control is-invalid")
+      $("#numberError").text("Please enter a number.")
+    } else {
+      var userName = $('input#name').val();
+      var highNumberInput = parseInt($("input#highNumber").val());
+      var numberList = []
 
+      var listing = function(number){
+        for (var i = 0; i <= number; i++){
+          numberList.push(i);
+        }
+      }
+      listing(highNumberInput);
+      stringInsert(numberList);
+
+      var finalList = numberList.map(function(number){
+        if (((typeof number) === 'string') && ((number).indexOf('Dave') !== -1)){
+          number = 'I\'m sorry ' + userName + '. I\'m afraid I can\'t do that.';
+          return number;
+        } else{
+          return number;
+        }
+      });
+
+      finalList.forEach(function(item){
+        $("form#counter").hide();
+        $("#result").fadeIn();
+        if ($("#order").val() === "prepend"){
+          $("ul.numberList").prepend('<li>' + item + '</li>');
+        } else if ($("#order").val() === "append"){
+          $("ul.numberList").append('<li>' + item + '</li>');
+        }
+      });
     }
-
-    var userName = $('input#name').val();
-    var highNumberInput = parseInt($("input#highNumber").val());
-    var numberList = []
-
-    var listing = function(number){
-      for (var i = 0; i <= number; i++){
-        numberList.push(i);
-      }
-    }
-    listing(highNumberInput);
-    stringInsert(numberList);
-
-    var finalList = numberList.map(function(number){
-      if (((typeof number) === 'string') && ((number).indexOf('Dave') !== -1)){
-        number = 'I\'m sorry ' + userName + '. I\'m afraid I can\'t do that.';
-        return number;
-      } else{
-        return number;
-      }
-    });
-
-    finalList.forEach(function(item){
-      $("form#counter").hide();
-      $("#result").fadeIn();
-      if ($("#order").val() === "prepend"){
-        $("ul.numberList").prepend('<li>' + item + '</li>');
-      } else if ($("#order").val() === "append"){
-        $("ul.numberList").append('<li>' + item + '</li>');
-      }
-    });
   });
 
 
