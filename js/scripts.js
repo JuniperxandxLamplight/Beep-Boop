e23e74f77523136805b2246a388c5fc1701712c6
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-01-25 09:40:39 -0800

    add string replacement for 1, 2, and 3 with priority for higher number

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
-var string
+var stringInsert = function(array){
+  array.forEach(function(number){
+    if (((number+"").indexOf('3')) !== -1){
+      array[number] = 'I\'m sorry Dave. I\'m afraid I can\'t do that.';
+    } else if (((number+"").indexOf('2')) !== -1){
+      array[number] = 'beep!';
+    } else if (((number+"").indexOf('1')) !== -1){
+      array[number] = 'boop!';
+    }
+  });
+  console.log(array);
+}
 $(function(){
 
   $("form#counter").submit(function(event){
     event.preventDefault();
     var highNumberInput = parseInt($("input#highNumber").val());
     var numberList = []
-    
+
     var listing = function(number){
       for (var i = 0; i <= number; i++){
         numberList.push(i);
       }
     }
     listing(highNumberInput);
+    stringInsert(numberList);
 
     $(".numberList").text(numberList.join(" "));
   });
